4da2612ec4213e8d532a783efe0383e158e44ba4
pxnx <loool1912@web.de>
2022-07-06 21:53:56 +0200

    actix works

--- src/db.rs
+++ src/db.rs
@@ -1,41 +1,26 @@
-use std::env;
-
+use crate::{errors::MyError, models::User};
 use deadpool_postgres::Client;
-use sqlx::{PgConnection, PgPool, Pool, Postgres};
-use sqlx::postgres::PgPoolOptions;
 use tokio_pg_mapper::FromTokioPostgresRow;
-use crate::error::MyError;
 
-use crate::model::{InputUser, User};
+pub async fn add_user(client: &Client, user_info: User) -> Result<User, MyError> {
+    let _stmt = include_str!("../sql/add_user.sql");
+    let _stmt = _stmt.replace("$table_fields", &User::sql_table_fields());
+    let stmt = client.prepare(&_stmt).await.unwrap();
 
-
-
-pub async fn get_pool() -> PgPool {
-
-    let db_url = env::var("DATABASE_URL").unwrap();
-
-    PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&*String::from(db_url))
-        .await.unwrap()
-}
-
-
-pub async fn add_user(conn: &PgPool, user: InputUser) -> Result<User, sqlx::Error>{ //Result<User, MyError> {
-sqlx::query_as!(User,
-        r#"INSERT INTO users(id, msg, date) VALUES ($1, $2, current_date) RETURNING id, msg, date;"#, user.id, user.msg
-    )
-        .fetch_one(&*conn)
+    client
+        .query(
+            &stmt,
+            &[
+                &user_info.email,
+                &user_info.first_name,
+                &user_info.last_name,
+                &user_info.username,
+            ],
+        )
         .await?
-
-
-
-
- /*       .iter()
+        .iter()
         .map(|row| User::from_row_ref(row).unwrap())
         .collect::<Vec<User>>()
         .pop()
         .ok_or(MyError::NotFound) // more applicable for SELECTs
-
-  */
 }
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+mod db;
+mod config;
+mod handlers;
+mod models;
+mod errors;
+
+use crate::config::ExampleConfig;
+use ::config::Config;
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use handlers::add_user;
+use tokio_postgres::NoTls;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let config_ = Config::builder()
+        .add_source(::config::Environment::default())
+        .build()
+        .unwrap();
+
+    let config: ExampleConfig = config_.try_deserialize().unwrap();
+
+    let pool = config.pg.create_pool(None, NoTls).unwrap();
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .service(web::resource("/users").route(web::post().to(add_user)))
+    })
+        .bind(config.server_addr.clone())?
+        .run();
+    println!("Server running at http://{}/", config.server_addr);
+
+
+
+
+    server.await
+}
