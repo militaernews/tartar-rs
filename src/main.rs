4b16ad1d9bbdf75f4d1ebdaa2ccfae1f3ac4be1e
pxnx <loool1912@web.de>
2022-07-10 22:00:06 +0200

    webhook not functional yet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::env;
 use std::error::Error;
-
+use std::{convert::Infallible, env, net::SocketAddr};
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use warp::Filter;
+use reqwest::{StatusCode, Url};
 
 use std::time::Duration;
 
@@ -21,6 +24,8 @@ mod models;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+    pretty_env_logger::init();
+    log::info!("Starting tartaros-telegram...");
 
     let bot: AutoSend<Bot> = Bot::from_env().auto_send();
 
@@ -28,10 +33,8 @@ async fn main() {
         .max_connections(5)
         .connect(&*env::var("DATABASE_URL").expect("DATABASE_URL must be provided!")).await.unwrap();
 
-
     let handler = dptree::entry()
         .branch(Update::filter_callback_query().endpoint(callback_handler));
-
 
     let b = bot.clone();
     let p = pool.clone();
@@ -39,7 +42,7 @@ async fn main() {
     tokio::spawn(async move {
         loop {
             println!("hm");
-            send_report(&b, &p).await;
+            send_report(&b, &p).await.unwrap();
             sleep(Duration::from_millis(5000)).await;
         }
     });
@@ -125,4 +128,66 @@ async fn send_report(
 
 
     Ok(())
+}
+
+use teloxide::{
+    dispatching::{
+        stop_token::AsyncStopToken,
+        update_listeners::{self, StatefulListener},
+    },
+    prelude::*,
+    types::Update,
+};
+
+
+async fn handle_rejection(error: warp::Rejection) -> Result<impl warp::Reply, Infallible> {
+    log::error!("Cannot process the request due to: {:?}", error);
+    Ok(StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+pub async fn webhook(bot: AutoSend<Bot>) -> impl update_listeners::UpdateListener<Infallible> {
+    // Heroku auto defines a port value
+    let teloxide_token = env::var("TELOXIDE_TOKEN").expect("TELOXIDE_TOKEN env variable missing");
+    let port: u16 = env::var("PORT")
+        .expect("PORT env variable missing")
+        .parse()
+        .expect("PORT value to be integer");
+    // Heroku host example .: "heroku-ping-pong-bot.herokuapp.com"
+    let host = env::var("HOST").expect("have HOST env variable");
+    let path = format!("bot{teloxide_token}");
+    let url = Url::parse(&format!("https://{host}/{path}")).unwrap();
+
+    bot.set_webhook(url).await.expect("Cannot setup a webhook");
+
+    let (tx, rx) = mpsc::unbounded_channel();
+
+    let server = warp::post()
+        .and(warp::path(path))
+        .and(warp::body::json())
+        .map(move |update: Update| {
+            tx.send(Ok(update)).expect("Cannot send an incoming update from the webhook");
+
+            StatusCode::OK
+        })
+        .recover(handle_rejection);
+
+    let (stop_token, stop_flag) = AsyncStopToken::new_pair();
+
+    let addr = format!("0.0.0.0:{port}").parse::<SocketAddr>().unwrap();
+    let server = warp::serve(server);
+    let (_addr, fut) = server.bind_with_graceful_shutdown(addr, stop_flag);
+
+    // You might want to use serve.key_path/serve.cert_path methods here to
+    // setup a self-signed TLS certificate.
+
+    tokio::spawn(fut);
+    let stream = UnboundedReceiverStream::new(rx);
+
+    fn streamf<S, T>(state: &mut (S, T)) -> &mut S {
+        &mut state.0
+    }
+
+    StatefulListener::new((stream, stop_token), streamf, |state: &mut (_, AsyncStopToken)| {
+        state.1.clone()
+    })
 }
